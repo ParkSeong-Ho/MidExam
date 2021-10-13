853f7d5207c3bbe30f9dd7dea263861b81bf1e6d
성호 박 <qkrtjdgh8040@gmail.com>
2021-10-13 22:49:18 +0900

    알고리즘 수정

--- Project1/소스.c
+++ Project1/소스.c
@@ -230,42 +230,357 @@
 //
 //
 //}
+//#include<stdio.h>
+//#pragma warning(disable:4996)
+//int main() {
+//	int gcd = 0;
+//	int primenumber[100] = { 0 };
+//	int num1;
+//	int i = 0;
+//	int dir = 0, count = 0;
+//	scanf("%d", &num1);
+//	for (int i = 1; i <= num1; i++)
+//	{
+//
+//		for (int j = 1; j <= i; j++)
+//		{
+//			count++;
+//			if (i % j == 0)
+//			{
+//				gcd++;
+//			}
+//
+//		}
+//		if (gcd == 2)
+//		{
+//			primenumber[dir] = i;
+//			dir++;
+//
+//		}
+//		gcd = 0;
+//	}
+//
+//	for (int c = 0; c < dir; c++)
+//	{
+//		printf("%d\n", primenumber[c]);
+//
+//	}
+//	printf("연산횟수 %d", count);
+//
+//
+//
+//
+//
+//
+//}
+//#include<stdio.h>
+//#pragma warning(disable:4996)
+//int main() {
+//	int gcd = 0;
+//	int primenumber[100] = { 0 };
+//	int num1;
+//	int i = 0;
+//	int dir = 0, count = 0;
+//	scanf("%d", &num1);
+//	for (int i = 1; i <= num1; i++)
+//	{
+//
+//		for (int j = 1; j <= i; j++)
+//		{
+//			count++;
+//			if (i % j == 0)
+//			{
+//				gcd++;
+//			}
+//
+//		}
+//		if (gcd == 2)
+//		{
+//			primenumber[dir] = i;
+//			dir++;
+//
+//		}
+//		gcd = 0;
+//	}
+//
+//	for (int c = 0; c < dir; c++)
+//	{
+//		printf("%d\n", primenumber[c]);
+//
+//	}
+//	printf("연산횟수 %d", count);
+//
+//
+//
+//
+//
+//
+//}
+//#include<stdio.h>
+//#pragma warning(disable:4996)
+//int main() {
+//	int gcd = 0;
+//	int primenumber[100] = { 0 };
+//	int num1;
+//	int i,j = 2;
+//	int dir = 0, count = 0,numbercount=0;
+//	scanf("%d", &num1);
+//	if (num1>=2)
+//	{
+//		printf("2\n");
+//	}
+//	for (int i = 3; i <= num1; i++)
+//	{
+//		gcd = 0;
+//		j = 2;
+//		while (gcd==0&&j<i)
+//			//while문은 for문과 다른다 이사람아 제발 똑바로 보라고 
+//		{
+//			count++;
+//			if (i % j == 0)
+//			{
+//				gcd++;
+//				break;
+//			}
+//			j++;
+//		}
+//		if (gcd == 0)
+//		{
+//			primenumber[dir] = i;
+//			dir++;
+//
+//		}
+//		
+//	}
+//
+//	for (int c = 0; c < dir; c++)
+//	{
+//		printf("%d\n", primenumber[c]);
+//
+//	}
+//	printf("연산횟수 %d", count);
+//
+//
+//
+//
+//
+//
+//}
+//#include<stdio.h>
+//int main() {
+//	int i, j, count, num, numbercount = 0;
+//	scanf_s("%d", &num);
+//	if (num >= 2) printf("2\n");
+//	for (i = 3; i <= num; i++) {
+//		count = 0;
+//		j = 2;
+//		while (count == 0 && j < i)
+//		{
+//			numbercount++;
+//			if (i % j == 0) {
+//				count++;
+//				break;
+//
+//			}
+//			j++;
+//		}
+//
+//
+//		if (count == 0)
+//		{
+//			printf_s("%d\n", i);
+//		}
+//
+//	}
+//	printf("계산 돌린 횟수 :%d", numbercount);
+//	return 0;
+//
+//
+//}
+
+//
+//}#include<stdio.h>
+//#pragma warning(disable:4996)
+////알고리즘 3번 소수에서 짝수는 2말고 존재하지 않는다 이것을 기준을 개발
+//int main() {
+//	int gcd = 0;
+//	int primenumber[100] = { 0 };
+//	int num1;
+//	int i, j = 2;
+//	int dir = 0, count = 0, numbercount = 0;
+//	scanf("%d", &num1);
+//	if (num1 >= 2)
+//	{
+//		printf("2\n");
+//	}
+//	for (int i = 3; i <= num1; i+=2)
+//	{
+//		gcd = 0;
+//		j = 2;
+//		while (gcd == 0 && j < i)
+//			
+//		{
+//			count++;
+//			if (i % j == 0)
+//			{
+//				gcd++;
+//				break;
+//			}
+//			if (j==2)
+//			{
+//				j++;
+//				count--;
+//			}
+//			else
+//			{
+//				j += 2;
+//			}
+//		}
+//		if (gcd == 0)
+//		{
+//			primenumber[dir] = i;
+//			dir++;
+//
+//		}
+//
+//	}
+//
+//	for (int c = 0; c < dir; c++)
+//	{
+//		printf("%d\n", primenumber[c]);
+//
+//	}
+//	printf("연산횟수 %d", count);
+//
+//
+//
+//
+//
+
+//#include<stdio.h>
+//#include<math.h>
+//#pragma warning(disable:4996)
+////알고리즘 4번 우리는 소수를 구할때 굳이 그수까지 나눠보지 않아도 된다 그수의 루트값이 나뉘어진다는것은 그수도 나누어 떨어진다는 뜻이다.
+//int main() {
+//	int gcd = 0;
+//	int primenumber[100] = { 0 };
+//	int num1;
+//	int i, j = 2;
+//	int dir = 0, count = 0, numbercount = 0;
+//	scanf("%d", &num1);
+//	if (num1 >= 2)
+//	{
+//		printf("2\n");
+//	}
+//	for (int i = 3; i <= num1; i += 2)
+//	{
+//		gcd = 0;
+//		j = 2;
+//		
+//		while (gcd == 0 && j <= sqrt(i))
+//			 
+//		{
+//			count++;
+//			if (i % j == 0)
+//			{
+//				gcd++;
+//				break;
+//			}
+//			if (j == 2)
+//			{
+//				j++;
+//				count--;
+//			}
+//			else
+//			{
+//				j += 2;
+//			}
+//		}
+//		if (gcd == 0)
+//		{
+//			primenumber[dir] = i;
+//			dir++;
+//
+//		}
+//
+//	}
+//
+//	for (int c = 0; c < dir; c++)
+//	{
+//		printf("%d\n", primenumber[c]);
+//
+//	}
+//	printf("연산횟수 %d", count);
+//
+//
+//
+//
+//
+//
+//}
+
+
 #include<stdio.h>
+#include<math.h>
 #pragma warning(disable:4996)
+//알고리즘 5번 우리는 소수를 구할때 굳이 그수까지 나눠보지 않아도 된다 그수의 루트값이 나뉘어진다는것은 그수도 나누어 떨어진다는 뜻이다.
+//거기서 더 추가하여 우리 소수를 구한값을 통하여 나누어서 나누어떨어진다면 소수라는 하나의 조건을 더추가하였다 이 알고리즘은 
 int main() {
-	int gcd = 0;
-	int primenumber[100] = { 0 };
-	int num1;
-	int i = 0;
-	int dir = 0, count = 0;
-	scanf("%d", &num1);
-	for (int i = 1; i <= num1; i++)
-	{
 
-		for (int j = 1; j <= i; j++)
+	int ar[1000] = { 0,0,0,0,0,0 };
+	int i, count = 0, k, num, numbercount = 0, ccount, j;
+	scanf_s("%d", &num);
+	j = 0;
+	ar[0] = 2;
+	k = 0;
+	for (i = 3; i <= num; i += 2)
+	{
+		j = 0;
+		ccount = 0;
+		while (ar[j] <= sqrt((float)i) && ar[j] != 0)
 		{
-			count++;
-			if (i % j == 0)
+			numbercount++;
+			if (i % ar[j] == 0)
 			{
-				gcd++;
+				ccount++;
+				break;
 			}
-
+			if (ar[j] == 2)
+			{
+				numbercount--;
+			}
+			j++;
 		}
-		if (gcd == 2)
+
+
+
+
+
+		if (ccount == 0)
 		{
-			primenumber[dir] = i;
-			dir++;
+			k++;
+			ar[k] = i;
+			k = k + 1;
+			ar[k] = 0;
+			k--;
 
 		}
-		gcd = 0;
-	}
 
-	for (int c = 0; c <= dir; c++)
+
+
+	}
+	int c = 0;
+
+	while (ar[c] != 0)
 	{
-		printf("%d\n", primenumber[c]);
-
+		printf("%d\n", ar[c]);
+		c++;
 	}
-	printf("연산횟수 %d", count);
+	printf("계산 돌린 횟수 : % d", numbercount);
+
+
+
+
 
 
 
