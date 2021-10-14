a67ec172b98d3345d51573511f0b8b1a63b865a1
성호 박 <qkrtjdgh8040@gmail.com>
2021-10-15 03:53:56 +0900

    gcd smallnumber algorithm commit please don't forget num1,num2 togetder /

--- Project1/소스.c
+++ Project1/소스.c
@@ -1,193 +1,4 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#pragma warning(suppress : 4996)
-//#define _CRT_SECURE_NO_WARNINGS
-//
-//#define MAX_SIZE 100
-//#define MAZE_SIZE 10
-//
-//typedef struct Mouse {
-//	int row;
-//	int col;
-//} Mouse;
-//
-//typedef struct Stack {
-//	Mouse data[MAX_SIZE];
-//	int top;
-//} Stack;
-//
-//char maze[MAZE_SIZE][MAZE_SIZE];
-//
-//int mazeRow = 0; int mazeCol = 0;
-//
-//void readMaze(char string[]) {
-//	FILE* fp;
-//	fp = fopen(string, "r");
-//
-//	while (!feof(fp)) {
-//		char c = fgetc(fp);
-//		if (c == '\n') {
-//			mazeRow++; mazeCol = 0;
-//		}
-//		else maze[mazeRow][mazeCol++] = c;
-//	}
-//	fclose(fp);
-//	mazeRow++; mazeCol--;
-//}
-//
-//void init(Stack* p) {
-//	p->top = -1;
-//}
-//
-//int isFull(Stack* p) {
-//	return (p->top == MAX_SIZE - 1);
-//}
-//
-//int isEmpty(Stack* p) {
-//	return (p->top == -1);
-//}
-//
-//void push(Stack* p, Mouse data) {
-//	if (isFull(p))
-//	{
-//		printf("Stack is full.\n");
-//		return;
-//	}
-//	else {
-//		p->top++;
-//		p->data[p->top].row = data.row;
-//		p->data[p->top].col = data.col;
-//	}
-//}
-//
-//Mouse pop(Stack* p) {
-//	if (isEmpty(p)) {
-//		printf("Stack is empty.\n");
-//		exit(1);
-//	}
-//	return p->data[(p->top)--];
-//}
-//
-//void pushTrace(Stack* s, int x, int y) {
-//	if (x<0 || y<0 || x>MAZE_SIZE || y>MAZE_SIZE) {
-//		return;
-//	}
-//	else if (maze[x][y] != '1' && maze[x][y] != '.') {
-//		Mouse tmp;
-//		tmp.row = x;
-//		tmp.col = y;
-//
-//		push(s, tmp);
-//	}
-//}
-//
-//void drawMap() {
-//	for (int row = 0; row < mazeRow; row++) {
-//		for (int col = 0; col < mazeCol; col++) {
-//			if (maze[row][col] == '0') {
-//				printf("□");
-//			}
-//			else if (maze[row][col] == '1') {
-//				printf("■");
-//			}
-//			else {
-//				printf("%c ", maze[row][col]);
-//			}
-//		}
-//		printf("\n");
-//	}
-//}
-//
-//int init_mouse(Mouse* m) {
-//	for (int row = 0; row < MAZE_SIZE; row++) {
-//		for (int col = 0; col < MAZE_SIZE; col++) {
-//			if (maze[row][col] == 'M') {
-//				m->row = row;
-//				m->col = col;
-//				return 0;
-//			}
-//		}
-//	}
-//	printf("couldn't find an  entrance: fail\n");
-//	exit(1);
-//}
-//
-//void appendMaze() {
-//	FILE* fp;
-//	fp = fopen("trace.txt", "a");
-//	if (fp == NULL) {
-//		puts("파일을 찾을 수 없습니다.");
-//		exit(1);
-//	}
-//	for (int row = 0; row < mazeRow; row++) {
-//		for (int col = 0; col < mazeCol; col++) {
-//			fputc(maze[row][col], fp);
-//		}
-//		fputc('\n', fp);
-//	}
-//	fclose(fp);
-//}
-//
-//int main(int argc, char* argv[]) {
-//	if (argc != 2) {
-//		puts("파일 열기에 실패했습니다.\n");
-//		return 1;
-//	}
-//	Stack s;
-//	Mouse m;
-//	int col, row, x, y;
-//	FILE* fp;
-//	fp = fopen("trace.txt", "w");
-//	if (fp == NULL) {
-//		printf("파일을 만들 수 없습니다.\n");
-//		exit(1);
-//	}
-//
-//	readMaze(argv[1]);
-//
-//	init(&s);
-//
-//	init_mouse(&m);
-//
-//	printf("미로 \n");
-//	drawMap();
-//
-//	printf("\n이동 경로\n");
-//	printf("시작 (%d, %d) -> ", m.row, m.col);
-//	fprintf(fp, "시작 (%d, %d) -> ", m.row, m.col);
-//
-//	while (maze[m.row][m.col] != 'X') {
-//		x = m.row;
-//		y = m.col;
-//
-//		maze[x][y] = '.';
-//
-//		pushTrace(&s, x + 1, y);
-//		pushTrace(&s, x - 1, y);
-//		pushTrace(&s, x, y + 1);
-//		pushTrace(&s, x, y - 1);
-//
-//		if (isEmpty(&s)) {
-//			printf("\n이동 경로를 찾을 수 없습니다. 실패\n");
-//			drawMap();
-//			return 0;
-//		}
-//		else {
-//			m = pop(&s);
-//			printf("(%d, %d) -> ", m.row, m.col);
-//			fprintf(fp, "(%d, %d) -> ", m.row, m.col);
-//		}
-//	}
-//	printf("도착\n");
-//	fprintf(fp, "도착\n");
-//	fclose(fp);
-//
-//	drawMap();
-//	printf("탐색 성공\n");
-//	appendMaze();
-//
-//	return 0;
-//}
+
 
 //#include<stdio.h>
 //#pragma warning(disable:4996)
@@ -634,7 +445,7 @@
 //		if (number2 % j == 0)
 //		{
 //			arrcount2++;
-//			num2[arrcount2] = j;
+//			num2[arrcount2] =j;
 //			
 //		}
 //
@@ -679,43 +490,159 @@
 //
 //	return 0;
 //}
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main() {
+//	int N, i, count, k=0, m=0, n=0, flag = 0;
+//	int number[100] = {2};
+//	
+//	scanf_s("%d", &N);
+//
+//	if (N >= 2)printf("2\n");
+//	for (i = 3;  i<= N; i+=2) {
+//		count = 0;
+//		while (number[k] <= sqrt((float)(i)) && number[k] != 0)
+//		{
+//			if (i % number[k] == 0) { 
+//				count++;
+//				break;
+//
+//			}
+//
+//			k++;
+//			
+//		}
+//			
+//			
+//		
+//		if (count == 0) {
+//			m++;
+//			printf("%d\n", i);
+//			number[m] = i;
+//			m = m+ 1;
+//			number[m] =  0 ;
+//			m--;
+//
+//
+//
+//		}
+//		flag = 0;
+//	}
+//}
 #include <stdio.h>
-#include <math.h>
+#pragma warning(disable:4996)
 
 int main() {
-	int N, i, count, k=0, m=0, n=0, flag = 0;
-	int number[100] = {2};
-	
-	scanf_s("%d", &N);
-
-	if (N >= 2)printf("2\n");
-	for (i = 3;  i<= N; i+=2) {
-		count = 0;
-		while (number[k] <= sqrt((float)(i)) && number[k] != 0)
-		{
-			if (i % number[k] == 0) { 
-				count++;
-				break;
-
-			}
-
-			k++;
-			
-		}
-			
-			
-		
-		if (count == 0) {
-			m++;
-			printf("%d\n", i);
-			number[m] = i;
-			m = m+ 1;
-			number[m] =  0 ;
-			m--;
-
-
-
-		}
-		flag = 0;
+	int gcdnumber[100];
+	int smallcount=0,smallnumber,gcdnum;
+	int number1, number2,reset=0;
+	int gcd=1;
+	scanf_s("%d", &number1);
+	scanf_s("%d", &number2);
+	while (reset<=99)
+	{
+		gcdnumber[reset] = 0;
+		reset++;
 	}
+	reset = 0;
+	if (number1>number2)
+	{
+		smallnumber = number2;
+	}
+	else
+	{
+		smallnumber = number1;
+	
+	}
+	for (int i = 2; smallnumber != 1; i++)
+	{
+		
+		while (smallnumber % i == 0)
+		{
+			
+			gcdnumber[smallcount] = i;
+			smallnumber=smallnumber / i;
+			/*printf("%d %d\n", smallcount, gcdnumber[smallcount]);*/
+			
+		}
+		smallcount++;
+	}
+	smallcount = 0;
+	for (int i = 0; gcdnumber[i] !=0; i++)
+	{
+		while (number1 %gcdnumber[i]==0 && number2 %gcdnumber[i]==0)
+		{
+			gcd = gcd * gcdnumber[i];
+			number1 = number1 / gcdnumber[i];
+			number2 = number2 / gcdnumber[i];
+
+		}
+	}
+	printf("%d", gcd);
+
+
+
 }
+
+//#include <stdio.h>
+//
+//int main() {
+//	//최대공약수 알고리즘 2번
+//	int i = 0, num = 0, num1, num2;
+//	int ar[100] = { 0 };
+//	int count = 0;
+//	int arrcount = 0;
+//	int GCD = 1;
+//	scanf_s("%d", &num1);
+//	scanf_s("%d", &num2);
+//	if (num1 > num2)
+//	{
+//		num = num2;
+//	}
+//	else
+//	{
+//		num = num1;
+//	}
+//	for (i = 2; num != 1; i++)
+//	{
+//		count++;
+//		if (num % i != 0) continue;
+//		while (num % i == 0)
+//		{
+//			count++;
+//			num /= i;
+//			if (ar[arrcount] == i)
+//			{
+//				count--;
+//			}
+//			ar[arrcount] = i;
+//		}
+//
+//		arrcount++;
+//	}
+//	for (int j = 0; ar[j] != 0; j++)
+//	{
+//		count++;
+//		while ((num1 % ar[j] == 0) && (num2 % ar[j] == 0))
+//		{
+//
+//			GCD = GCD * ar[j];
+//			num1 = num1 / ar[j];
+//			num2 = num2 / ar[j];
+//
+//		}
+//
+//	}
+//	printf("최소공배수%d\n", GCD);
+//	printf("계산횟수%d", count);
+//
+//
+//
+//
+//
+//
+//
+//	return 0;
+//}
+
